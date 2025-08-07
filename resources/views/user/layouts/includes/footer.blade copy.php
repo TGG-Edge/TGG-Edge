26cf3ebd0d813fcb3c0ae031f351db8f18fa6337
Sweety-Mitra <sweety.mitra2002@gmail.com>
2025-08-07 22:57:51 +0530

    changes

---
+++ resources/views/user/layouts/includes/footer.blade copy.php
@@ -0,0 +1,72 @@
+ <footer id="colophon" class="site-footer site-footer-two has-footer-bg mt-1">
+        <section class="footer-top">
+            <div class="container">
+                <div class="row py-1">
+
+                    <div class="col-lg-4 col-md-4 footer-links">
+                        <!-- <img class="ftr_logo" src="assets/dist/images/logo.jpg"> -->
+                        <h3>ABOUT US</h3>
+                        <p>TGG Foundation is a self-sustaining, not-for-profit, registered Public Charitable Trust with
+                            a Global Vision. It works for the welfare of the rural community especially women, youths
+                            and elderly people through its Sustainable Rural Development Goals (TSRDG). It also provides
+                            opportunities for people from across the world to participate in their transformation
+                            journey by assuming greater responsibilities and being accountable to their own actions,
+                            this is accomplished through its Responsible Human Mission (RHM) Program. </p>
+                    </div>
+
+                    <div class="col-lg-4 col-md-4 footer-links">
+                        <h3>REGISTRATIONS & CERTIFICATIONS</h3>
+                        <p>TRUST : 69/IV/152/2014
+                        <p>
+                        <p>12A: AACTT4554A/09/14-15/T-0195</p>
+                        <p>80G: AACTT4554A/09/16-17T-0016/80G</p>
+                        <p>FCRA: 052980177</p>
+                        <p>FCRA: 052980177</p>
+                        <p>NITI AYOG (DARPAN) : KL/2017/0117389</p>
+                        <p>CSR-1: CSR00009975.</p>
+                        <p>GUIDESTAR INDIA (TRANSPARENCY): 10456</p>
+                    </div>
+
+                    <div class="col-lg-4 col-md-4 footer-links">
+                        <h3>IMPORTANT LINKS</h3>
+                        <ul>
+                            <li><a href="https://tggfct.org/privacy-policy/">Privacy Policy</a></li>
+                            <li><a href="https://tggfct.org/terms-and-conditions/">Terms & Conditions</a></li>
+                            <li><a href="https://tggfct.org/shipping-and-delivery/">Shipping & Delivery</a></li>
+                            <li><a href="https://tggfct.org/cancellation-and-refund/">Cancellation & Refund</a></li>
+                            <li><a href="https://tggfct.org/gallary/">Gallery</a></li>
+                        </ul>
+
+                        <div id="section-g3fc223"
+                            class="wp-block-gutentor-e2 section-g3fc223 gutentor-element gutentor-element-button button-align-left-desktop">
+                            <span class="gutentor-button-wrap"><a
+                                    class="gutentor-button gutentor-block-button gutentor-icon-after"
+                                    href="http://tggfct.org/download/1974/" style="padding: 10px;
+                            text-decoration: none;"><span>Activity Report <i
+                                            class="gutentor-button-icon fa fa-download"></i></span></a></span></div>
+                    </div>
+
+                    <div class="col-lg-2 col-md-2 footer-links">
+                        <h3>ADDRESS</h3>
+                        <p>Wayanad Country Homes,Onneyar, Vaduvanchal-P.O Wayanad,Kerala, India-673581</p>
+                    </div>
+
+                </div>
+            </div>
+
+        </section>
+
+        <div class="bottom-footer">
+            <div class="container py-10" style="padding-top: 40px; padding-bottom: 30px;">
+                <!-- social links area -->
+                <!-- social links area -->
+                <div class="footer-menu"><!-- Footer Menu-->
+                    <div class="site-info">All Rights Reserved.&nbsp; TGG Foundation Charitable Trust ©2024.&nbsp;
+                        Designed By&nbsp;<a href="https://www.tggindia.com" > TGG India&nbsp;&nbsp;</a> &nbsp;&nbsp; <i></i> &nbsp; &nbsp; &nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    </div><!-- .site-info -->
+                </div><!-- footer Menu-->
+
+            </div>
+        </div>
+    </footer>
